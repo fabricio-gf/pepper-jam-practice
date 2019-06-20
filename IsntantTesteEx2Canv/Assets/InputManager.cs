76a4de1b5f2463077c53ff37a9c57c3fc3a05208
Gabriel Simmel Nascimento <galbrato@gmail.com>
2019-06-20 20:50:34 -0300

    Teste exporta 2 canvas

---
+++ IsntantTesteEx2Canv/Assets/InputManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+    //inside class
+    static Vector2 firstPressPos;
+    static Vector2 secondPressPos;
+    static Vector2 currentSwipe;
+
+    public static Vector2 CurrentSwipe {
+        get {
+            Swipe();
+            return currentSwipe;
+        }
+    }
+    private void Update() {
+        Swipe();
+    }
+
+    static void Swipe() {
+
+        if (Input.GetMouseButtonDown(0)) {
+            //save began touch 2d point
+            firstPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            //save ended touch 2d point
+            secondPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+            //create vector from the two points
+            currentSwipe = new Vector2(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+
+            //normalize the 2d vector
+            currentSwipe.Normalize();
+
+            //swipe upwards
+            if (currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f) {
+                Debug.Log("up swipe");
+            }
+            //swipe down
+            if (currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f) {
+                Debug.Log("down swipe");
+            }
+            //swipe left
+            if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f) {
+                Debug.Log("left swipe");
+            }
+            //swipe right
+            if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f) {
+                Debug.Log("right swipe");
+            }
+        }
+    }
+}
+
---
+++ IsntantTesteEx2Canv/Assets/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    Vector3 dir;
+    float delay = 0.1f;
+    float timer;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.W)) {
+            dir.x = 0f; dir.y = 1f;
+        }
+        if (Input.GetKeyDown(KeyCode.S)) {
+            dir.x = 0f; dir.y =-1f;
+        }
+        if (Input.GetKeyDown(KeyCode.D)) {
+            dir.x = 1f; dir.y = 0f;
+        }
+        if (Input.GetKeyDown(KeyCode.A)) {
+            dir.x =-1f; dir.y = 0f;
+        }
+
+        timer += Time.deltaTime;
+        if (timer > delay) {
+            timer = 0;
+            transform.position += dir;
+            dir.x = 0; dir.y = 0;
+        }
+
+    }
+
+
+}
