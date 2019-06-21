b8fe709f72fc84850576668ddc34229829b9bbf3
Fabricio <fabricio-gf@hotmail.com>
2019-06-21 20:21:59 -0300

    Final push

---
+++ Assets/Scripts/Game/ReorderLayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReorderLayer : MonoBehaviour
+{
+    SpriteRenderer mrender;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mrender = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mrender.sortingOrder = Mathf.CeilToInt((transform.parent.position.y + 10) * 100);
+    }
+}
